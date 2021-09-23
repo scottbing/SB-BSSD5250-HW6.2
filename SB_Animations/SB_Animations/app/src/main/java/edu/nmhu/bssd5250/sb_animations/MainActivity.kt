9ce46eff6b4502f90138459c4c52ba03d4cb64dc
scottbing <bingscott@gmail.com>
2021-09-23 16:56:06 -0600

    Hwk6.2
    Hwk6.2

--- SB_Animations/SB_Animations/app/src/main/java/edu/nmhu/bssd5250/sb_animations/MainActivity.kt
+++ SB_Animations/SB_Animations/app/src/main/java/edu/nmhu/bssd5250/sb_animations/MainActivity.kt
@@ -13,16 +13,22 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-
+import java.lang.Math.round
 
 
 class MainActivity : AppCompatActivity() {
     private var bugAnimation: AnimationDrawable? = null
     private var mothAnimation: AnimationDrawable? = null
+    private var birdAnimation: AnimationDrawable? = null
+    private var monkeyAnimation: AnimationDrawable? = null
     private var bugMover: ObjectAnimator? = null
     private var mothMover: ObjectAnimator? = null
+    private var birdMover: ObjectAnimator? = null
+    private var monkeyMover: ObjectAnimator? = null
     private var bugView: ImageView? = null
     private var mothView: ImageView? = null
+    private var birdView: ImageView? = null
+    private var monkeyView: ImageView? = null
     private var textView: TextView? = null
     private var contentView: ViewGroup? = null
     private var startAnimationButton: Button? = null
@@ -83,13 +89,48 @@ class MainActivity : AppCompatActivity() {
             }
             false
         }
+
+        // make the bird view
+        birdView = ImageView(this@MainActivity)
+        birdView!!.setImageResource(R.drawable.bird_animation)
+        //birdView.setBackgroundResource(R.drawable.bird_animation);
+        birdView!!.setOnTouchListener { _: View?, event: MotionEvent ->
+            // TODO Auto-generated method stub
+            Log.i("imageviewandontouchlistener", "imageView1 onTouch")
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                birdMover!!.cancel()
+                birdMover!!.end()
+                return@setOnTouchListener true
+            }
+            false
+        }
+
+        // make the monkey view
+        monkeyView = ImageView(this@MainActivity)
+        monkeyView!!.setImageResource(R.drawable.monkey_animation)
+        //monkeyView.setBackgroundResource(R.drawable.monkey_animation);
+        monkeyView!!.setOnTouchListener { _: View?, event: MotionEvent ->
+            // TODO Auto-generated method stub
+            Log.i("imageviewandontouchlistener", "imageView1 onTouch")
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                monkeyMover!!.cancel()
+                monkeyMover!!.end()
+                return@setOnTouchListener true
+            }
+            false
+        }
+
         bugAnimation = bugView!!.drawable as AnimationDrawable
         mothAnimation = mothView!!.drawable as AnimationDrawable
+        birdAnimation = birdView!!.drawable as AnimationDrawable
+        monkeyAnimation = monkeyView!!.drawable as AnimationDrawable
         textView = TextView(this@MainActivity)
         textView!!.text = "0"
         contentView!!.addView(textView)
         contentView!!.addView(bugView)
         contentView!!.addView(mothView)
+        contentView!!.addView(birdView)
+        contentView!!.addView(monkeyView)
 
         // get screen dimensions
         val width = baseContext.resources.displayMetrics.widthPixels
@@ -117,13 +158,39 @@ class MainActivity : AppCompatActivity() {
             // mothAnimation.start();
         }
 
+        //start bird animation
+        if (birdAnimation!!.isRunning) birdAnimation!!.stop() else {
+            birdMover = ObjectAnimator.ofFloat(birdView, "translationX", 500f)
+            birdMover?.duration = 1000
+            birdMover?.repeatCount = ValueAnimator.RESTART
+            birdMover?.repeatMode = ValueAnimator.REVERSE
+            //birdMover.start();
+            // birdAnimation.start();
+        }
+
+        //start monkey animation
+        if (monkeyAnimation!!.isRunning) monkeyAnimation!!.stop() else {
+            monkeyMover = ObjectAnimator.ofFloat(monkeyView, "translationY", 500f)
+
+            /*monkeyMover = ObjectAnimator.ofFloat(monkeyView, "translationX", 500f).apply {
+                addUpdateListener { updateAnimation ->
+                    monkeyView!!.translationY = updateAnimation.animatedValue as Float
+            }*/
+
+            monkeyMover?.duration = 1000
+            monkeyMover?.repeatCount = ValueAnimator.RESTART
+            monkeyMover?.repeatMode = ValueAnimator.REVERSE
+            //monkeyMover.start();
+            // monkeyAnimation.start();
+        }
+
         // group the animators inot an AnimationSet
         val set = AnimatorSet()
-        set.playTogether(bugMover, mothMover)
+        set.playTogether(bugMover, mothMover, birdMover, monkeyMover)
         set.start()
 
         //count from  0 to  100
-        val animator = ValueAnimator.ofInt(30, 0)
+        val animator = ValueAnimator.ofInt(500, 0)
         animator.reverse() // count down
         //do it 1000  milliseconds
         animator.duration = 1000
@@ -138,12 +205,12 @@ class MainActivity : AppCompatActivity() {
             if (valueAnimator.animatedValue.toString().toInt() == 0) {
                 mothAnimation!!.stop()
                 bugAnimation!!.stop()
-                if (!mothAnimation!!.isRunning && !bugAnimation!!.isRunning) {
-                    Toast.makeText(this@MainActivity, "Game Over - You Won!", Toast.LENGTH_LONG)
-                        .show()
+                birdAnimation!!.stop()
+                monkeyAnimation!!.stop()
+                if (!mothAnimation!!.isRunning && !bugAnimation!!.isRunning && !birdAnimation!!.isRunning) {
+                    Toast.makeText(this@MainActivity, "Game Over - You Won!", Toast.LENGTH_LONG).show()
                 } else {
-                    Toast.makeText(this@MainActivity, "Game Over - You Lost", Toast.LENGTH_LONG)
-                        .show()
+                    Toast.makeText(this@MainActivity, "Game Over - You Lost", Toast.LENGTH_LONG).show()
                 }
             }
         }
